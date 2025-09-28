b81dabade1414a371f4aa07ca08452120df3f565
schnorea <aussiev123@yahoo.com>
2025-09-28 00:34:51 -0400

    Changed a sample file.

--- test/long_short.cpp
+++ test/long_short.cpp
@@ -7,4 +7,12 @@ int main(void) {
     long long x = 5;
     long long y = 10;
     long long z = x + y; // 'x' and 'y' here are DECL_REF_EXPR
+
+    if (x > 0) {      // A
+        y = x + 1;    // B
+        z = y * 2;    // C
+    } else {
+        y = x - 1;    // D
+        z = y * 2;    // E
+    }
 }
